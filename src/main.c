98df8bad3e849150bd908d2dc7109721fb0887ec
jvol1 <jvol@cin.ufpe.br>
2021-04-29 22:01:00 -0300

    Sistema de colisao adicionado
    Implementei o sistema de colisao

--- src/main.c
+++ src/main.c
@@ -1,21 +1,15 @@
 #include "raylib.h"
+#define MAX_OBSTACLES 28
 
 typedef struct {
-    Vector2 position;
+    Rectangle rec;
     Texture2D texture;
 } Player;
 
 typedef struct {
-    int x;
-    int y;
-}Frame;
-
-
-typedef struct {
-    bool destructible;
-    Vector2 position;
-    int tam;
-}Obstacle;
+    Rectangle rec;
+    Texture2D texture;
+} Obstacle;
 
 
 int main(void){
@@ -23,45 +17,31 @@ int main(void){
     const int screenHeight = 1080;
 
     Player player;
-    Obstacle obstacles[30];
-
+    Obstacle obstacles[25];
 
     InitWindow(screenWidth, screenHeight, "IP - THE GAME");
 
-    // NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
-    player.texture = LoadTexture("C:\\Users\\Acer\\Desktop\\GAME IP\\assets\\moniMonstro1.png"); // Texture loading // Hard Coded 
-    Texture2D obstacleTexture = LoadTexture("C:\\Users\\Acer\\Desktop\\GAME IP\\assets\\caixa.png");
+    // As textura tem que ser inicializadas na main
+    // Textures devem ser carregadas depois da inicializacao da Janela 
+    player.texture = LoadTexture("C:\\Users\\Acer\\Desktop\\BomberMan\\assets\\moniMonstro1.png");
+    Texture2D obstacleTexture = LoadTexture("C:\\Users\\Acer\\Desktop\\BomberMan\\assets\\caixa.png");
+
+
+    InitPlayer(&player);
+    InitObstacles(obstacles);
 
 
     Rectangle frameRec = { 0.0f, 0.0f, (float)player.texture.width/2, (float)player.texture.height/4};
-    Frame currentFrame = {0 , 0};
+    Vector2 currentFrame = {0 , 0};
     int framesCounter = 0;
     int framesSpeed = 3;            // Number of spritesheet frames shown by second
 
-    SetTargetFPS(30);
+    SetTargetFPS(60);
 
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
         framesCounter++;
-
-        if(IsKeyDown(KEY_RIGHT)){
-
-            player.position.x += 5.0f;
-            currentFrame.y = 0;       
-        }
-        if(IsKeyDown(KEY_LEFT)){
-            player.position.x -= 5.0f;
-            currentFrame.y = 1;       
-        }
-        if(IsKeyDown(KEY_DOWN)){
-            player.position.y += 5.0f;
-            currentFrame.y = 2;       
-        }
-        if(IsKeyDown(KEY_UP)){
-            player.position.y -= 5.0f;
-            currentFrame.y = 3; 
-        }
-
+        UpdateGame(&player, obstacles, screenWidth, screenHeight);
 
         if (framesCounter >= (30/framesSpeed)){
             framesCounter = 0;
@@ -73,34 +53,95 @@ int main(void){
             frameRec.x = (float) currentFrame.x * (float) player.texture.width / 2;
             frameRec.y = (float) currentFrame.y * (float) player.texture.height / 4;
         }
-
-        /*
-        // Wall behaviour
-        if (player.rec.x <= 0) player.rec.x = 0;
-        if (player.rec.x + player.rec.width >= screenWidth) player.rec.x = screenWidth - player.rec.width;
-        if (player.rec.y <= 0) player.rec.y = 0;
-        if (player.rec.y + player.rec.height >= screenHeight) player.rec.y = screenHeight - player.rec.height;
-        */
        
-        // Draw
-        //----------------------------------------------------------------------------------
+
+
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
 
-            DrawTextureRec(player.texture, frameRec, player.position, WHITE);  // Draw part of the texture
-            DrawTexture(obstacleTexture, obstacles[0].position.x, obstacles[0].position.y, WHITE);
+            // DrawTextureRec(player.texture, frameRec, (Vector2) {player.rec.x, player.rec.y}, RAYWHITE);  // Draw part of the texture
+            DrawRectangle(player.rec.x, player.rec.y, 100, 100, RED);
+
+            for(int i = 0; i < MAX_OBSTACLES; i++)
+                DrawRectangle(obstacles[i].rec.x, obstacles[i].rec.y, 128, 128, BLUE);
 
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    UnloadTexture(player.texture);       // Texture unloading
 
-    CloseWindow();                // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+
+    UnloadTexture(player.texture);
+    UnloadTexture(obstacleTexture);
+
+    CloseWindow();
 
     return 0;
+}
+
+void UpdateGame(Player *playerPtr, Obstacle *obstacles, int screenWidth, int screenHeight){
+    // Atualiza o jogo a cada frame
+
+    if(IsKeyDown(KEY_RIGHT)){
+        playerPtr->rec.x += 5;
+        if(CheckCollisionMultipleRecs(*playerPtr, obstacles) == true)
+            playerPtr->rec.x -= 5;
+    }
+
+    if(IsKeyDown(KEY_LEFT)){
+        playerPtr->rec.x -= 5;
+        if(CheckCollisionMultipleRecs(*playerPtr, obstacles) == true)
+            playerPtr->rec.x += 5;
+    }
+
+    if(IsKeyDown(KEY_UP)){
+        playerPtr->rec.y -= 5;
+        if(CheckCollisionMultipleRecs(*playerPtr, obstacles) == true)
+            playerPtr->rec.y += 5;
+    }
+
+    if(IsKeyDown(KEY_DOWN)){
+        playerPtr->rec.y += 5;
+        if(CheckCollisionMultipleRecs(*playerPtr, obstacles) == true)
+            playerPtr->rec.y -= 5;
+    }
+
+
+    if (playerPtr->rec.x <= 0) playerPtr->rec.x = 0;
+    if (playerPtr->rec.x + playerPtr->rec.width >= screenWidth) playerPtr->rec.x = screenWidth - playerPtr->rec.width;
+    if (playerPtr->rec.y <= 0) playerPtr->rec.y = 0;
+    if (playerPtr->rec.y + playerPtr->rec.height >= screenHeight) playerPtr->rec.y = screenHeight - playerPtr->rec.width;    
+}
+
+void InitPlayer(Player *playerPtr){
+    // Inicializa o player 
+
+    playerPtr->rec = (Rectangle) {0, 0, 100, 100};
+}
+
+
+void InitObstacles(Obstacle *obstacles){
+    int i, j, k;
+
+    k = 0;
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 7; j++){
+            obstacles[k].rec.x = 128 + 256 * j;
+            obstacles[k].rec.y = 128 + 256 * i;
+            obstacles[k].rec.height = 128;
+            obstacles[k].rec.width = 128;
+            k++;
+        }
+    }
+}
+
+
+int CheckCollisionMultipleRecs(Player player, Obstacle *obstacle){
+    int i;
+
+    for(i = 0; i < MAX_OBSTACLES; i++){
+        if(CheckCollisionRecs(obstacle[i].rec, player.rec) == true)
+            return true;
+    }
+    return false;
 }
